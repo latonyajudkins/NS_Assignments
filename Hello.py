68f782d9b3ae361267d2967df773c9f8a69909f0
latonyajudkins <50683021+latonyajudkins@users.noreply.github.com>
2019-06-17 12:58:27 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Hello Tonya </title>
+	<!--<link rel="stylesheet" type="text/css" href="Styles/bootstrap-citrus.css">-->
+</head>
+<body>
+	<h1> SourceDigtal</h1>
+	<h2> SourceDigtal</h2>
+	<button>Back</button>
+	<!--<h3> SourceDigtal</h3>
+	<h4> SourceDigtal</h4>-->
+	
+	<h5> SourceDigtal</h5>
+	<h6> SourceDigtal</h6>
+
+	<p>SourceDigital is a Marketing Agency founded in 2010 as a new business venture within i.c.stars. We have a team of 10 professionals with deep expertise and passion for helping clients increase lead flow, reduce customer acquisition costs, and connect with target customers. </p>
+	<ul>Menu
+		<li><a href="http://myjuci" target="_self">Home</a></li>
+		<li><a href="https://myjuci.com/pages/about-our-store" target="_self">About</a></li>
+		<li><a href="https://myjuci.com/pages/faq" target="blank">faq</a></li>
+		<li><a href="https://myjuci.com" target="_self">Click here to subscribe</a></li></ul>
+
+	<ul>The List
+		<li>Students</li>
+		<li>Designs</li>
+		<li>Pets</li></ul>
+
+	<ol>The List
+		<li>Students</li>
+		<li>Designs</li>
+		<li>Pets</li></ol>
+	<img src="images/Lion.jpeg">
+
+	<p>SourceDigital is a Marketing Agency founded in 2010 as a new business venture within i.c.stars. We have a team of 10 professionals with deep expertise and passion for helping clients increase lead flow, reduce customer acquisition costs, and connect with target customers. </p>
+	<img src="images/house.png">
+
+</body>
+</html>
